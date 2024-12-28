d736fe648b787416b63a67770311fbdf8ef2e1ef
Pythondevopover <farhodjonergashov2010@gmail.com>
2024-12-28 17:38:29 +0500

    f

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://partfolio-7oau.onrender.com/"]
+ALLOWED_HOSTS = ["render.com"]
 
 
 # Application definition

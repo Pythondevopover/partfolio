c6f51ea6576f53fe0e281de1abe8ccb8397aa680
Pythondevopover <farhodjonergashov2010@gmail.com>
2024-12-28 17:59:06 +0500

    f

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import about
+from django.shortcuts import render
 
+def custom_page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
+handler404 = custom_page_not_found
 urlpatterns = [
     path('', about, name='about'),  # Bosh sahifa yoki About sahifasi
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import blog_list, blog_detail
+from django.shortcuts import render
 
+def custom_page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
+handler404 = custom_page_not_found
 urlpatterns = [
     path('', blog_list, name='blog_list'),  # Bloglar ro'yxati
     path('<int:post_id>/', blog_detail, name='blog_detail'),  # Blog post detallari

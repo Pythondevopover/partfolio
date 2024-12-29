c16440e4fee8c4bcbf3873813a713c317ce0f10e
Pythondevopover <farhodjonergashov2010@gmail.com>
2024-12-29 16:57:22 +0500

    f

--- staticfiles/js/snow.js
+++ staticfiles/js/snow.js
@@ -5,7 +5,7 @@
       snowflake.className = 'snowflake';
       snowflake.textContent = '❄';
       snowflake.style.left = Math.random() * 100 + 'vw';
-      snowflake.style.animationDuration = Math.random() * 9 + 8 + 's'; // 7-15 soniya oralig'ida
+      snowflake.style.animationDuration = Math.random() * 12 + 10 + 's'; // 7-15 soniya oralig'ida
       snowflake.style.fontSize = Math.random() * 10 + 10 + 'px';
       snowContainer.appendChild(snowflake);
     }

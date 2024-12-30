ad79a27a64cbb3cd2d90928141226a178e8af121
Pythondevopover <farhodjonergashov2010@gmail.com>
2024-12-30 23:06:59 +0500

    f

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.force404.Force404Middleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
